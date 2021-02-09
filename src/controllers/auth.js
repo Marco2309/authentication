825c7b6f471b4e92aa79e55e8a0587fd8fb10ead
Marco <trujas_2309@hotmail.com>
2021-02-08 22:02:29 -0600

    cambios en pc madrina

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,12 +1,17 @@
 import { Users } from "../models/";
 import { success, error } from '../network/response'
 
+export const defaultt = async (req, res) => {
+  res.send('Server Run successfully')
+}
 //1. Completar la logica para manejar el inicio de sesión
 // - responder con un codigo de estado 401 cuando las credenciales sean incorrectas
 // - responder con un mensaje (message) y codigo de estado 200 cuando las credenciales sean correctas
 // - responder con el token jwt (token) 
 export const login = async (req, res) => {
-
+  try {
+    
+  } catch (error) { error(req, res, e, 400) }
 }
 
 //2. Completar el registro de usuario
@@ -21,10 +26,16 @@ export const signIn = async (req, res) => {
 }
 
 export const users = async (req, res) => {
-
+try {
+  const userCreated = await Users.create(datos);
+    success(req, res, 201, userCreated)
+} catch (e) { error(req, res, e, 400) }
 }
 
 export const user = async (req, res) => {
-
+try {
+  const userCreated = await Users.create(datos);
+    success(req, res, 201, userCreated)
+} catch (e) { error(req, res, e, 400) }
 }
 
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,7 @@
 import express from "express";
-import {login, signIn, users, user} from "../controllers/auth";
+import {defaultt, login, signIn, users, user} from "../controllers/auth";
 const router = express.Router();
-
+router.get("/", defaultt);
 router.post("/login", login);
 router.post("/signin", signIn);
 router.get("/users", users);
